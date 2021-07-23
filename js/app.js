166a2723eb64ac2daf2ed2434cbc3dfb02a46679
Milan Hira <48389651+milanhira@users.noreply.github.com>
2021-07-23 22:52:36 +0600

    some line was changed

---
+++ js/app.js
@@ -0,0 +1,94 @@
+// Some js code practices
+
+
+// console.log('Hello World');
+// console.error("This is the error message");
+// // console.warn("This is a warning");
+// const score =21;
+// console.log(score);
+// const name= 'Milan Hira';
+// const age=25;
+// console.log('My name is'+' '+name+' '+'and I am'+' '+age);
+// // console.log(`MYname is ${name} and I am ${age}`);
+// const stringText = "Milan is a good fullstack web developer Now";
+// console.log(stringText.toLowerCase());
+// console.log(stringText.substring(0,5));
+// console.log(stringText.split('').reverse().toString());
+// const s = 'technology, cumputers, it, code';
+// console.log(s.split(', '));
+// const numbers = new Array(1,2,3,4,5);
+// console.log(numbers);
+// const fruits = ['apple', 'oranges', 'pears', 10, true];
+// console.log(fruits);
+// fruits[5]= "fullstact Milan";
+// fruits.push('banana');
+// console.log(fruits);
+// fruits.unshift('strawberries');
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// console.log(Array.isArray(fruits));
+// console.log(Array.isArray('banana'));
+// console.log(fruits.indexOf('apple'));
+
+// const persone = {
+//   firstName: 'Milan',
+//   lastName: 'Hira',
+//   age: 25,
+//   hobbis: ['become Motivetor', 'writer', 'poiettric'],
+//   address: {
+//     street: 'Dakatia, 150/len',
+//     city: 'Chitalmary',
+//     state: 'NA'
+//   }
+  
+// }
+// console.log(persone.address.city);
+
+// //ert(persone);
+// console.log(persone);
+// const {firstName, lastName, age, hobbis} = persone;
+// console.log(firstName);
+// console.log(age);
+// console.log(hobbis);
+// hobbis.push('video mekar');
+// console.log(hobbis);
+// hobbis.pop();
+// console.log(hobbis);
+// console.log(Array.isArray('video mekar'));
+// hobbis.unshift('Milan will be a full stact web developer one day');
+// console.log(hobbis);
+// console.log(hobbis.indexOf('poiettric'));
+// console.log(hobbis.length);
+// hobbis[5] = "HI full stact web developer";
+// console.log(hobbis);
+// persone.email = "13milanhira@gmail.com";
+// console.log(persone);
+
+// const hello = [ 
+//   {
+//     id: 1,
+//     text: 'Take out trash',
+//     isCompleted: true
+//   },
+//   {
+//     id: 2,
+//     text: 'Metting wlith boss',
+//     isCompleted: true
+//   },
+//   {
+//     id: 3,
+//     text: 'etails tell me now',
+//     isCompleted: false
+//   }
+// ];
+
+// console.log(hello.length);
+// console.log(hello[0].text);
+
+// const myData = {id, text, isCompleted} = hello;
+
+// console.log(myData);
+
+
+
